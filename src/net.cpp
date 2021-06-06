2b188502738ba0ac8ca0cf1660275717af407557
amilton <amiltonbh@gmail.com>
2021-06-06 17:52:59 -0300

    update commit

--- src/net.cpp
+++ src/net.cpp
@@ -1173,11 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"monexcointools.com", "dnsseed.monexcointools.com"},
-    {"monexcoinpool.org", "dnsseed.monexcoinpool.org"},
-    {"xurious.com", "dnsseed.ltc.xurious.com"},
-    {"koin-project.com", "dnsseed.koin-project.com"},
-    {"weminemnc.com", "dnsseed.weminemnc.com"},
+    {"monexcoin.org", "190.109.67.22"},
     {NULL, NULL}
 };
 
